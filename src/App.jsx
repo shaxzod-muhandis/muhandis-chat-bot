c03cc2ea7dda8fe8f751da8da0c4f5555e736542
shaxzod <shaxzodmuhandis@gmail.com>
2024-06-11 21:36:44 +0500

    first commit start project

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Muhandis Chat bot</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+      <p className="read-the-docs">
+         Click Muhandis counter
+      </p>
+    </>
+  )
+}
+
+export default App
